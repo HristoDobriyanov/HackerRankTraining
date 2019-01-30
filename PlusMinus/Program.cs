d4830a4a7425af6d94812900ce3c1e9fea80d036
Hristo Dobriyanov <hdobriyanov@gmail.com>
2019-01-30 14:59:38 +0200

    Updates

--- PlusMinus/Program.cs
+++ PlusMinus/Program.cs
@@ -10,14 +10,47 @@ namespace PlusMinus
     {
         static void plusMinus(int[] arr)
         {
+            Decimal pos = 0;
+            Decimal neg = 0;
+            Decimal[] ret = new decimal[3];
 
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    pos++;
+                }
+                else if (arr[i] < 0)
+                {
+                    neg++;
+                }
+
+            }
+            ret[0] = pos / arr.Length;
+            ret[1] = neg / arr.Length;
+            ret[2] = (arr.Length - pos - neg) / arr.Length;
+
+            foreach (var item in ret)
+            {
+                Console.WriteLine(item);
+            }
 
         }
 
 
         static void Main(string[] args)
         {
-            
+            int[] arr = new int[] { 1, 1, 0, -1, -1 };
+
+            plusMinus(arr);
+
+            Decimal some = 2;
+            Decimal other = 5;
+
+            Decimal res = some / other;
+
+            Console.WriteLine(res);
+
         }
     }
 }
